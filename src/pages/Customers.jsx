5b738d1bff82a2d31370e5b003984666639e7f40
FaisalDipto <dipto22d@gmail.com>
2024-04-04 23:44:33 +0600

    Add customers page Implemented new customer page to facilitate user management

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        id='gridcomp'
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => {
+            console.log("Item", item); // Log the item object
+            return (
+              <ColumnDirective
+                key={index} {...item}
+              />
+            );
+          })}
+        </ColumnsDirective>
+        <Inject services={[ Page, Toolbar, Selection, Edit, Sort, Filter ]} />
+      </GridComponent>
+    </div>
   )
 }
 
