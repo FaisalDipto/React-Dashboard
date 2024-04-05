37fa2309808ffdf85e1dc5f289cbc0fcd7a4ff10
FaisalDipto <dipto22d@gmail.com>
2024-04-05 12:21:43 +0600

    Add Calender - An app to track dates and events

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className='m-2 md:10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="App" title="Calender"/>
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2024, 3, 1)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
+      </ScheduleComponent>
+    </div>
   )
 }
 
