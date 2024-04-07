242039cd221148a7103504530cde1384beefc57c
FaisalDipto <dipto22d@gmail.com>
2024-04-07 20:55:52 +0600

    Fix Typo

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ const App = () => {
                 <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* Charts */}
-                <Route path="/lines" element={<Line />} />
+                <Route path="/line" element={<Line />} />
                 <Route path="/area" element={<Area />} />
                 <Route path="/bar" element={<Bar />} />
                 <Route path="/pie" element={<Pie />} />
