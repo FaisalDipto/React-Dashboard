f8039d2762ccd052640a17d5357ec1d514dd9480
FaisalDipto <dipto22d@gmail.com>
2024-04-07 21:13:30 +0600

    Fix bug - Make it class based component(Recommended)

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  // Map the data to transform each object to the expected format
-  const formattedData = data.map(item => ({ x: item.x, y: item.yval }));
-console.log("Inside Sparkline: ", width);
-console.log("Inside Sparkline Data:",  data);
-  const Output = (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={formattedData}
-      xName="x"
-      yName="y"
-      type={type}
-      tooltipSettings={{
-        visible: true,
-        format: '${x} : ${y}',
-        trackLineSettings: {
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
           visible: true,
-        }
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  );
-  console.log("Output: ", Output);
-  return Output;
+          // eslint-disable-next-line no-template-curly-in-string
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
 }
 
 export default SparkLine;
